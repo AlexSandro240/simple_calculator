d0e6003924e479286dc956c4ce3f8d5941fdb207
AlexSandro240 <githubsandro.240@gmail.com>
2023-04-07 01:37:47 -0300

    Projeto simple calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+function inserir(num){
+    document.querySelector('.tela').innerHTML += num;
+}
+
+function limpar(){
+    document.querySelector('.tela').innerHTML = '';
+}
+
+function voltar(){
+    let tela = document.querySelector('.tela').innerHTML;
+    document.querySelector('.tela').innerHTML = tela.substring(0, tela.length-1);
+}
+
+function resultado(){
+    let res = document.querySelector('.tela').innerHTML;
+    if(res){
+        document.querySelector('.tela').innerHTML = eval(res).toLocaleString(1);
+    }else{
+        document.querySelector('.tela').innerHTML = 'Nenhum comando...';
+    }
+}
